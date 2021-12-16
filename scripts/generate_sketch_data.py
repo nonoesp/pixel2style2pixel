12943dfa9737b2e93f31074f551017282c3fea8c
Nono Martínez Alonso <mundowarezweb@gmail.com>
2021-12-16 18:57:41 +0100

    Accept arguments in generate_sketch_data.py.

--- scripts/generate_sketch_data.py
+++ scripts/generate_sketch_data.py
@@ -4,6 +4,7 @@ from torch.utils.serialization import load_lua
 import os
 import cv2
 import numpy as np
+import argparse
 
 """
 NOTE!: Must have torch==0.4.1 and torchvision==0.2.1
@@ -35,28 +36,44 @@ def get_sketch_image(image_path):
     return sketch_image[:, :, np.newaxis]
 
 
-use_cuda = True
+def simplify(input_dir, output_dir, model):
 
-cache = load_lua("/path/to/sketch_gan.t7")
-model = cache.model
-immean = cache.mean
-imstd = cache.std
-model.evaluate()
+    print('Input ›', input_dir)
+    print('Output ›', output_dir)
+    print('Model ›', model)
 
-data_path = "/path/to/data/imgs"
-images = [os.path.join(data_path, f) for f in os.listdir(data_path)]
+    use_cuda = True
 
-output_dir = "/path/to/data/edges"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    cache = load_lua(model)
+    model = cache.model
+    immean = cache.mean
+    imstd = cache.std
+    model.evaluate()
 
-for idx, image_path in enumerate(images):
-    if idx % 50 == 0:
-        print("{} out of {}".format(idx, len(images)))
-    data = get_sketch_image(image_path)
-    data = ((transforms.ToTensor()(data) - immean) / imstd).unsqueeze(0)
-    if use_cuda:
-        pred = model.cuda().forward(data.cuda()).float()
-    else:
-        pred = model.forward(data)
-    save_image(pred[0], os.path.join(output_dir, "{}_edges.jpg".format(image_path.split("/")[-1].split('.')[0])))
+    images = [os.path.join(input_dir, f) for f in os.listdir(input_dir)]
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for idx, image_path in enumerate(images):
+        if idx % 50 == 0:
+            print("{} out of {}".format(idx, len(images)))
+        data = get_sketch_image(image_path)
+        data = ((transforms.ToTensor()(data) - immean) / imstd).unsqueeze(0)
+        if use_cuda:
+            pred = model.cuda().forward(data.cuda()).float()
+        else:
+            pred = model.forward(data)
+        save_image(pred[0], os.path.join(output_dir, "{}_edges.jpg".format(
+            image_path.split("/")[-1].split('.')[0])))
+
+    
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Generate sketch data from images.')
+    parser.add_argument('-i', '--input', required=True, help='Directory containing input images.')
+    parser.add_argument('-o', '--output', required=True, help='Directory containing output sketches.')
+    parser.add_argument('-m', '--model', required=True, help='Path to pre-trained model.')
+    opt = parser.parse_args()
+    
+    simplify(opt.input, opt.output, opt.model)
